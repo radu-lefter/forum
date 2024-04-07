517eff691994119dd6b0ff8ec5eeff373f1aa900
Radu Lefter <lefter_radu@yahoo.com>
2024-04-07 14:05:57 +0100

    linked index to individual posts

--- resources/js/Pages/Posts/Index.vue
+++ resources/js/Pages/Posts/Index.vue
@@ -2,8 +2,11 @@
     <AppLayout>
         <Container>
             <ul class="divide-y">
-                <li v-for="post in posts.data" :key="post.id" class="px-2 py-4">
-                    <span class="font-bold text-lg">{{ post.title }}</span>
+                <li v-for="post in posts.data" :key="post.id">
+                    <Link :href="route('posts.show', post.id)" class="block group px-2 py-4">
+                        <span class="font-bold text-lg group-hover:text-indigo-500">{{ post.title }}</span>
+                        <span class="first-letter:uppercase block pt-1 text-sm text-gray-600">{{ formattedDate(post) }} ago by {{ post.user.name }}</span>
+                    </Link>
                 </li>
             </ul>
 
@@ -15,6 +18,11 @@
 import AppLayout from "@/Layouts/AppLayout.vue";
 import Container from "@/Components/Container.vue";
 import Pagination from "@/Components/Pagination.vue";
+import {Link} from "@inertiajs/vue3";
+import {formatDistance, parseISO} from "date-fns";
+import {relativeDate} from "@/Utilities/date.js";
 
 defineProps(['posts']);
+
+const formattedDate = (post) => relativeDate(post.created_at);
 </script>
--- tests/Feature/Controllers/PostController/IndexTest.php
+++ tests/Feature/Controllers/PostController/IndexTest.php
@@ -2,18 +2,19 @@
 
 use App\Http\Resources\PostResource;
 use App\Models\Post;
-use Inertia\Testing\AssertableInertia;
 
 use function Pest\Laravel\get;
 
 it('should return the correct component', function () {
-    $this->get(route('posts.index'))
+    get(route('posts.index'))
         ->assertComponent('Posts/Index');
 });
 
 it('passes posts to the view', function () {
     $posts = Post::factory(3)->create();
 
-    $this->get(route('posts.index'))
+    $posts->load('user');
+
+    get(route('posts.index'))
         ->assertHasPaginatedResource('posts', PostResource::collection($posts->reverse()));
 });
